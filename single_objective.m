295bf59faccb763bfc1878abc57aa0b6698fc7c3
HillBrandon <bhill126@umd.edu>
2024-11-21 13:57:30 -0500

    Added eqs with efficiency

--- single_objective.m
+++ single_objective.m
@@ -1,6 +1,8 @@
 %-----------%
 % Equations % (script is not to be run) 
 %-----------%
+% Objectives: Power (at any moment), Time to commute 
+% Design Varis: Gear Ratio,  Tire Width, Effort
 
 % ==Trails== %
 W = x*crr*m*g*cosd(theta)
@@ -16,4 +18,6 @@ Pd = (1/2) * rho * V_roll^3 * Cd * A
 Ps = V_roll * m_total * g * s
 % Total power assembly, with user power 
 Pa==P_user - (Pd + Ps + Pr)
-
+% maybe rearrange to minimize power input from the user?
+% also, include effiency from the drive train that affects user power "eta"
+P_user == (Pa + Pd + Ps + Pr) / eta
