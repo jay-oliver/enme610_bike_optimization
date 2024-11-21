d50d77480222dda813a9d1d6e044f3b072e4b23c
HillBrandon <bhill126@umd.edu>
2024-11-21 13:37:08 -0500

    Added power equations

--- single_objective.m
+++ single_objective.m
@@ -1,6 +1,19 @@
 %-----------%
-% Equations %
+% Equations % (script is not to be run) 
 %-----------%
 
-% Trails %
+% ==Trails== %
 W = x*crr*m*g*cosd(theta)
+
+% ==Power Equivalence== %
+% Power for acceleration 
+Pa = V_roll * m_total * accel 
+% Power needed to roll 
+Pr = V_roll * m_total * g * Crr
+% Power to overcome drag
+Pd = (1/2) * rho * V_roll^3 * Cd * A
+% Power needed to go on a slope (s>0 means uphill)
+Ps = V_roll * m_total * g * s
+% Total power assembly, with user power 
+Pa==P_user - (Pd + Ps + Pr)
+
