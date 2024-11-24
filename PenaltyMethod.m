b214093e89ca5dd20479569a7eea3622d8a4aea8
HillBrandon <bhill126@umd.edu>
2024-11-23 23:30:07 -0500

    Continued typing

--- PenaltyMethod.m
+++ PenaltyMethod.m
@@ -10,21 +10,22 @@ f=@(x) (x(1)-1).^2 + (x(2)-2).^2;
 g1=@(x) x(1)+x(2)-2;
 % Penalty fxn is g(x)^2
 % Assuming an unfeasible point, exterior penalty method 
-ri=1; %Some initial r value 
-phi=@(x) f([x(1),x(2)]) + r*(g1([x(1),x(2)]))^2; 
+ri=1000; %Some initial r value 
+phi=@(x) f([x(1),x(2)]) + ri*(g1([x(1),x(2)]))^2; 
 
 % constrained for reference 
 [sx,sf]=fmincon(f,[0,0],[1,1],2)
 
 xi=[0,0]; % Doing an exterior penalty method, assuming r to be high
-options=optimset('MaxFunEval',100);
-[x,fx]=fminunc(phi,xi,options)
+[x,fx]=fminunc(phi,xi)
 
 % Actual function to start modifying the penalty value
 % BHJay_Ext_Penalty
 % (function, initial input, end_condition, adjustment factor)
-function [opt_x,opt_f]=BHJay_Ext_Penalty(func,xi,end_con,alpha)
+function [opt_x,opt_f]=BHJay_Ext_Penalty(func,p_func,xi,end_con,alpha)
     eta=end_con;
     a=alpha; 
-    
+    r=1;
+    phi=func+r*p_func;
+    [xi,fi]=fminunc(phi,xi)
 end
